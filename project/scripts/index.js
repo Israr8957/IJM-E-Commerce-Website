c281e47dde6ffd780b4ddf2af414d4344ed406e8
Israr8957 <159294828+Israr8957@users.noreply.github.com>
2024-02-16 00:56:25 +0530

    added file

---
+++ project/scripts/index.js
@@ -0,0 +1,44 @@
+let bagItems;
+onLoad();
+
+function onLoad() {
+  displayItemOnHomePage();
+  let bagItemsStr=localStorage.getItem('bagItems');
+  bagItems=bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayBagIcon();
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems',JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement=document.querySelector('.bag-item-count');
+  bagItemCountElement.innerText=bagItems.length;
+}
+  function displayItemOnHomePage() {
+    let itemsContainerElement=document.querySelector('.items-container');
+    if(!itemsContainerElement) {
+       return;
+    }
+    let innerHTML='';
+    items.forEach(item=>{
+      innerHTML+=`
+      <div class="item-container">
+        <img class="item-image" src="${item.image}" alt="image">
+        <div class="rating">
+       ${item.rating.stars}⭐ | ${item.rating.count} </div>
+       <div class="company-name">${item.company}</div>
+       <div class="item-name">${item.item_name}</div>
+       <div class="price">
+         <span class="current-price">${item.current_price}</span>
+         <span class="original-price">${item.original_price}</span>
+         <span class="discount">(${item.discount_percentage}% OFF)</span>
+       </div>
+       <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+     </div>`
+  });
+  itemsContainerElement.innerHTML=innerHTML;
+}
